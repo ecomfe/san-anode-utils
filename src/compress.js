a3bd65a13ac084393c9d564e61f09a1bed144b53
errorrik <errorrik@gmail.com>
2020-06-04 16:49:58 +0800

    add license

--- src/compress.js
+++ src/compress.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Baidu Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license.
+ * See LICENSE file in the project root for license information.
+ *
+ * @file ANode 压缩
+ */
 
 var ExprType = require('san').ExprType;
 
